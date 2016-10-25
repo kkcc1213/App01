9651f4cf724c4aeef489fb37ae6e89a9c60084d8
kkcc1213 <kkcc1213@gmail.com>
2016-10-25 15:01:04 +0700

    ทำไปก่อนพักเที่ยง

--- app/src/main/java/tlc/kkcc_pk/app01/MainActivity.java
+++ app/src/main/java/tlc/kkcc_pk/app01/MainActivity.java
@@ -2,12 +2,24 @@ package tlc.kkcc_pk.app01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText UsernameEditText, PasswordEditText;
+    private Button LoginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void StartNewForm() {
+        //Shift + Ctrl + Enter
+        //auto completed
+
     }
 }
